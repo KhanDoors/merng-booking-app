349df5f4f3bc9f29dc15909d14379173d566c9e8
unknown <syyadkhan@yahoo.com>
2019-01-04 14:29:12 -0500

    frontend routes setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,21 @@
 import React, { Component } from "react";
-
+import { BrowserRouter, Route, Redirect, Switch } from "react-router-dom";
 import "./App.css";
+import Auth from "./components/Auth";
+import Events from "./components/Events";
+import Bookings from "./components/Bookings";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <h1>whats up</h1>
-      </div>
+      <BrowserRouter>
+        <Switch>
+          <Redirect from="/" to="/auth" exact />
+          <Route path="/auth" component={Auth} />
+          <Route path="/events" component={Events} />
+          <Route path="/bookings" component={Bookings} />
+        </Switch>
+      </BrowserRouter>
     );
   }
 }
