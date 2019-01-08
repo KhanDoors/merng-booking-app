e6a5de5f3748149f074a8ab9480b69ecb43ccc09
unknown <syyadkhan@yahoo.com>
2019-01-07 19:56:54 -0500

    navbar took too long

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,26 @@
-import React, { Component } from "react";
+import React, { Component, Fragment } from "react";
 import { BrowserRouter, Route, Redirect, Switch } from "react-router-dom";
 import "./App.css";
 import Auth from "./components/Auth";
 import Events from "./components/Events";
 import Bookings from "./components/Bookings";
+import MainNavbar from "./components/Navbar/MainNavbar";
 
 class App extends Component {
   render() {
     return (
       <BrowserRouter>
-        <Switch>
-          <Redirect from="/" to="/auth" exact />
-          <Route path="/auth" component={Auth} />
-          <Route path="/events" component={Events} />
-          <Route path="/bookings" component={Bookings} />
-        </Switch>
+        <Fragment>
+          <MainNavbar />
+          <main className="main-content">
+            <Switch>
+              <Redirect from="/" to="/auth" exact />
+              <Route path="/auth" component={Auth} />
+              <Route path="/events" component={Events} />
+              <Route path="/bookings" component={Bookings} />
+            </Switch>
+          </main>
+        </Fragment>
       </BrowserRouter>
     );
   }
