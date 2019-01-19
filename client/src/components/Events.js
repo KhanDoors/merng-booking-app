05bd8acf8b4ddaa88bcbaad8967a08592292c752
unknown <syyadkhan@yahoo.com>
2019-01-19 12:46:37 -0500

    create booking modal complete

--- client/src/components/Events.js
+++ client/src/components/Events.js
@@ -1,11 +1,47 @@
-import React, { Component } from "react";
+import React, { Component, Fragment } from "react";
+import "./Events.css";
+import Modal from "./Modal/Modal";
+import Backdrop from "../components/Backdrop/Backdrop";
 
 class Events extends Component {
+  state = {
+    creating: false
+  };
+
+  startCreateEventHandler = () => {
+    this.setState({ creating: true });
+  };
+
+  modalConfirmHandler = () => {
+    this.setState({ creating: false });
+  };
+
+  modalCancelHandler = () => {
+    this.setState({ creating: false });
+  };
+
   render() {
     return (
-      <div>
-        <h1>Events Page</h1>
-      </div>
+      <Fragment>
+        {this.state.creating && <Backdrop />}
+        {this.state.creating && (
+          <Modal
+            title="Add Event"
+            canCancel
+            canConfirm
+            onCancel={this.modalCancelHandler}
+            onConfirm={this.modalConfirmHandler}
+          >
+            <p>Modal Content</p>
+          </Modal>
+        )}
+        <div className="events-control">
+          <p>Entire Your Special Event</p>
+          <button className="btn" onClick={this.startCreateEventHandler}>
+            Create Event
+          </button>
+        </div>
+      </Fragment>
     );
   }
 }
